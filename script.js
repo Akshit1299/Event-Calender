2bcf0dd17c99fa080ab597caac3e1700c1d679e3
Akshit Rajput <akshitrajput167@gmail.com>
2026-02-11 09:02:54 +0530

    started java and added variables

---
+++ script.js
@@ -0,0 +1,8 @@
+let events = [];
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("form");
+    const eventList = document.getElementById("eventList");
+    const card1 = document.querySelector(".card1");
+    const card2 = document.querySelector(".card2");
